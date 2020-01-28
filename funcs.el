f3e7856fe9a4e7e1debfb490d501e8ffa11491f9
roger <roger@Loopier.local>
2020-01-28 14:11:46 +0100

    Fix eval-within-parens.
    Change the VIM command to:
    1. Mark current position and  map it  to ‘z’.
    2. Find the enclosing parenthesis.
    3. Enter visual-mode.
    4. Select code block.
    5. Evaluate code.
    6. Enter normal-mode.
    7. Go back to marked position.

--- funcs.el
+++ funcs.el
@@ -51,5 +51,5 @@
 (defun sclang-evaluate-within-parens ()
   "Evaluate code within outermost parentheses (assumes active evil mode)"
   (interactive)
-  (setq unread-command-events (listify-key-sequence "vi(,,fd")))
+  (setq unread-command-events (listify-key-sequence "mz(v),,v`z")))
 
